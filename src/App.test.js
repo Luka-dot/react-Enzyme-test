5bab7786711e0d5419443efe58e23958f1f98024
Luka-dot <79lukask@gmail.com>
2020-11-10 16:04:17 -0500

    new repo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme from 'enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
